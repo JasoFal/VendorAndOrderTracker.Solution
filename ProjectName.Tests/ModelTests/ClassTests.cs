203b9dc54bd078d2c32dae2f4f80ee238067919a
JasoFal <jasonfalk9@gmail.com>
2023-12-06 13:42:18 -0800

    Initial commit

---
+++ ProjectName.Tests/ModelTests/ClassTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName.Models;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class ProjectTests
+  {
+    // Tests will go here!
+  }
+}
