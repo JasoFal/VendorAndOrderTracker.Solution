4c19e0d5bec1e4b2a61a356a8cc354abefef0896
Jason Falk <jasonfalk9@gmail.com>
2023-12-11 13:44:25 -0800

    Rename and add using statements to files.

---
+++ VendorAndOrderTracker/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProjectName
+{
+  class Program
+  {
+    static void Main(string[] arg)
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(arg);
+      builder.Services.AddControllersWithViews();
+      WebApplication app = builder.Build();
+      // app.UseDeveloperExceptionPage();
+      app.UseHttpsRedirection();
+      app.UseRouting();
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+      app.Run();
+    }
+  }
+}
