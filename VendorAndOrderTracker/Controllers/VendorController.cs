020703314780af747f1e616a3db5b7cdbc62c4ea
Jason Falk <jasonfalk9@gmail.com>
2023-12-15 15:16:45 -0800

    Change to match new variables in model.

--- VendorAndOrderTracker/Controllers/VendorController.cs
+++ VendorAndOrderTracker/Controllers/VendorController.cs
@@ -39,11 +39,11 @@ namespace VendorAndOrderTracker.Controllers
     }
 
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, double orderPrice, string orderDate)
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, double orderPrice, int orderMonth, int orderDay, int orderYear)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderMonth, orderDay, orderYear);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);
